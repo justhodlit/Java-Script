cd43f7c85cc00b3eb0317f34a9219421a616a70e
justhodlit <70793097+justhodlit@users.noreply.github.com>
2021-02-21 16:41:42 +0900

    Update index_arrowfunction.js

--- js/ex1/index_arrowfunction.js
+++ js/ex1/index_arrowfunction.js
@@ -8,8 +8,9 @@ const add = (a,b) => a+b;
 //1번째거 짧게 표현
 
 const hello = (name) => {
-  console.log('hello, ${name}!');
+  console.log(`hello, ${name}!`);
 }
 
 hello('Danny');
 // 두번째꺼 에러남... ${name} 똑같이 했는데 이부분에서 에러가 남
+// 이유 찾음. 따옴표가 아니라 물결표시 shift 누르기 전에 것이다. 즉 1번 옆에거로 괄호 안을 작성해줘야한다.
