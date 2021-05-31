e931e6ee61ad6d17b65c6b5818070c925f16f94a
justhodlit <70793097+justhodlit@users.noreply.github.com>
2021-05-31 18:51:19 +0900

    Add files via upload

---
+++ brocode_tutorial13/index.js
@@ -0,0 +1,27 @@
+/*
+    for loop = repeat a loop a limited amount of times
+               for(declare index; condition; step)
+*/
+
+for(let i = 0; i <= 10; i++){
+    console.log(i);
+}
+
+
+///////
+
+for(let i = 10; i > 0; i--){
+    console.log(i);
+}
+
+console.log("HAPPY NEW YEAR!");
+
+////////
+let i = 10;
+
+for(; i > 0;){
+    console.log(i);
+    i--;
+}
+
+console.log("HAPPY NEW YEAR!");
